766c42105c825a21ae35ec6e7383d022420ef6c2
KraZey <admin@krazey.de>
2013-12-07 01:06:53 +0100

    renamed variables, changed detecting pids, etc

--- ARMARE.bat
+++ ARMARE.bat
@@ -32,10 +32,9 @@ cls
 ::----------------------------------------------------------------------------------::
 set ARMA2=arma2oaserver.exe
 set STARTPATH=%cd%
-set COUNT=1
+set BECCOUNT=1
 set TIMERSTA=1
 set TIMEREND=20
-set PID=
 set var=1
 ::----------------------------------------------------------------------------------::
 ::-----detecting pid of this cmd-process--------------------------------------------::
@@ -148,23 +147,24 @@ if '%choice%' EQU 'N' goto detectpid
 if '%choice%' EQU '%var%' goto choice_var
 if '%choice%' EQU '%var%x' goto choice_varx
 :choice_y
+cd /d %becpath%
 if '%var%' GTR '%servernumm%' goto detectpid
 del %checkfile%%var% /q > nul
 set /a var=%var%+1 > nul
 goto choice_y
 :choice_var
 set var=%choice%
-for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "windowtitle eq %%server%var%windowtitle%%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDSERVER%var% set "PIDSERVER%var%=%%p")
-for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDBEC%var% set "PIDBEC%var%=%%p")
+for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "windowtitle eq %%server%var%windowtitle%%"') do (echo %PIDs% | find "%%p" > nul || if not defined pidserver%var% set "pidserver%var%=%%p")
+for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || if not defined pidbec%var% set "pidbec%var%=%%p")
 del %checkfile%%var% /q > nul
 goto startserver
 :choice_varx
 set var=%choice:~0,-1%
-for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "windowtitle eq %%server%var%windowtitle%%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDSERVER%var% set "PIDSERVER%var%=%%p")
-for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDBEC%var% set "PIDBEC%var%=%%p")
+for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "windowtitle eq %%server%var%windowtitle%%"') do (echo %PIDs% | find "%%p" > nul || if not defined pidserver%var% set "pidserver%var%=%%p")
+for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || if not defined pidbec%var% set "pidbec%var%=%%p")
 cls
-if defined PIDSERVER%var% echo useless > %checkfile%%var% && echo checkfile created! ..back to GUI.
-if not defined PIDSERVER%var% echo %%server%var%%% not online! ..back to GUI.
+if defined pidserver%var% echo useless > %checkfile%%var% && echo checkfile created! ..back to GUI.
+if not defined pidserver%var% call echo %%server%var%%% not online! ..back to GUI.
 timeout /t 2 /nobreak > nul
 goto gui
 :://////////////////////////////////////////////////////////////////////////////////::
@@ -172,25 +172,23 @@ goto gui
 :://////////////////////////////////////////////////////////////////////////////////::
 :detectpid
 if '%var%' GTR '%servernum%' goto finisheddetectpid
-::for /f "tokens=1* delims==" %%i in ('set server%var%') do echo %%j
-::call echo %%server%var%windowtitle%%
-::call echo %%server%var%%%
-if not defined PIDSERVER%var% timeout /t 1 /nobreak > nul
-if not defined PIDSERVER%var% echo --------------------------------------------------------------------------------
-if not defined PIDSERVER%var% for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "windowtitle eq %%server%var%windowtitle%%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDSERVER%var% set "PIDSERVER%var%=%%p")
-if defined PIDSERVER%var% call echo %%server%var%%% - PID: %%PIDSERVER%var%%%
-if defined PIDSERVER%var% timeout /t 1 /nobreak > nul
-if not defined PIDBEC%var% for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDBEC%var% set "PIDBEC%var%=%%p")
-if defined PIDBEC%var% call echo %%bec%var%%% - PID: %%PIDBEC%var%%%
-if defined PIDBEC%var% echo\
-if defined PIDBEC%var% echo --------------------------------------------------------------------------------
-if defined PIDBEC%var% timeout /t 1 /nobreak > nul
+echo --------------------------------------------------------------------------------
+if not defined pidserver%var% for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "windowtitle eq %%server%var%windowtitle%%"') do (echo %PIDs% | find "%%p" > nul || set "pidserver%var%=%%p")
+if defined pidserver%var% call echo %%server%var%%% - PID: %%pidserver%var%%%
+if defined pidserver%var% timeout /t 1 /nobreak > nul
+if not defined pidbec%var% for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || set "pidbec%var%=%%p")
+if defined pidbec%var% call echo %%bec%var%%% - PID: %%pidbec%var%%%
+echo\
+echo --------------------------------------------------------------------------------
+timeout /t 1 /nobreak > nul
 set /a var=%var%+1 > nul
 goto detectpid
 ::----------------------------------------------------------------------------------::
 ::-----fake detect, placeholder-----------------------------------------------------::
 ::----------------------------------------------------------------------------------::
 :finisheddetectpid
+if '%choice%' EQU 'Y' goto goto check
+if '%var%' GTR '%servernumm%' goto detectpid
 cd /d %becpath%
 set var=1
 cls
@@ -211,48 +209,47 @@ echo PIDs detected. Initialize checkfiles...
 timeout /t 5 /nobreak > nul
 cls
 echo --------------------------------------------------------------------------------
-goto check
 :://////////////////////////////////////////////////////////////////////////////////::
 :://///PROCESS CHECKFILES///////////////////////////////////////////////////////////::
 :://////////////////////////////////////////////////////////////////////////////////::
 :check
 if '%TIMERSTA%' GTR '%TIMEREND%' goto restartarmare
 if not exist %checkfile%%var% goto startserver
-if '%servernum%' EQU '%var%' goto counter
+if '%servernum%' EQU '%var%' goto BECCOUNTer
 set /a var=%var%+1 > nul
 goto check
-:counter
+:BECCOUNTer
 set var=1
 timeout /t 5 /nobreak > nul
 title %title% - TIMER: %TIMERSTA% / %TIMEREND%
-set /a COUNT=%COUNT%+1 > nul
+set /a BECCOUNT=%BECCOUNT%+1 > nul
 set /a TIMERSTA=%TIMERSTA%+1 > nul
-if '%COUNT%' EQU '10' goto BECCHECK
+if '%BECCOUNT%' EQU '10' goto beccheck
 goto check
 ::----------------------------------------------------------------------------------::
 ::-----bec-onlinecheck--------------------------------------------------------------::
 ::----------------------------------------------------------------------------------::
-:BECCHECK
-set COUNT=1
+:beccheck
+set BECCOUNT=1
 goto check
-::if '%becONchecker%' == 'OFF' set COUNT=1
-::if '%becONchecker%' == 'OFF' goto check
+::if '%becONchecker%' EQU 'OFF' set BECCOUNT=1
+::if '%becONchecker%' EQU 'OFF' goto check
 ::add varcheck for hive_error
 ::for /f "skip=3 tokens=2" %%p in ('tasklist /fi "windowtitle eq hive error"') do (echo %PIDs% | find "%%p" > nul || if not defined HIVE_ERROR set "HIVE_ERROR=%%p")
 ::if defined HIVE_ERROR timeout /t 2 /nobreak > nul
-::if defined HIVE_ERROR set COUNT=1
+::if defined HIVE_ERROR set BECCOUNT=1
 ::if not defined HIVE_ERROR goto BECCHECKjump
-::if '%HIVE_ERROR%' == '%PIDSERVER1%' goto startserver
+::if '%HIVE_ERROR%' EQU '%pidserver1%' goto startserver
 ::----------------------------------------------------------------------------------::
 ::BECCHECKjump
-::if defined PIDBEC1 tasklist /fi "PID eq %PIDBEC1%" | findstr %PIDBEC1% > nul
-::if defined PIDBEC1 if %errorlevel%==1 goto becoff1
-::if defined PIDBEC1 timeout /t 2 /nobreak > nul
-::set COUNT=1
+::if defined pidbec1 tasklist /fi "PID eq %pidbec1%" | findstr %pidbec1% > nul
+::if defined pidbec1 if %errorlevel%EQU1 goto becoff1
+::if defined pidbec1 timeout /t 2 /nobreak > nul
+::set BECCOUNT=1
 ::goto check
 ::----------------------------------------------------------------------------------::
 ::becoff1
-::set COUNT=1
+::set BECCOUNT=1
 ::if exist %checkfile%1 del %checkfile%1 /q
 ::if not exist %checkfile%1 goto becoffjump
 :://////////////////////////////////////////////////////////////////////////////////::
@@ -260,14 +257,13 @@ goto check
 :://////////////////////////////////////////////////////////////////////////////////::
 :startserver
 ::if defined server%var%offline call echo %%server%var%offline%%
-
 timeout /t 5 /nobreak > nul
-if defined PIDSERVER%var% call echo %date% %time:~0,8%	Kill %%server%var%%% Server + BEC
-if defined PIDSERVER%var% call taskkill /f /pid %%PIDSERVER%var%%% > nul
-if defined PIDBEC%var% call taskkill /f /pid %%PIDBEC%var%%% > nul
+if defined pidserver%var% call echo %date% %time:~0,8%	Kill %%server%var%%% Server + BEC
+if defined pidserver%var% call taskkill /f /pid %%pidserver%var%%% > nul
+if defined pidbec%var% call taskkill /f /pid %%pidbec%var%%% > nul
 timeout /t 1 /nobreak > nul
-if defined PIDSERVER%var% set "PIDSERVER%var%="
-if defined PIDBEC%var% set "PIDBEC%var%="
+if defined pidserver%var% set "pidserver%var%="
+if defined pidbec%var% set "pidbec%var%="
 if defined HIVE_ERROR set "HIVE_ERROR="
 timeout /t 1 /nobreak > nul
 for /f "delims=" %%l in ('for /f "skip=3 tokens=2" %%p in ^('tasklist /fi "imagename eq %ARMA2%"'^) do @^<nul set /p "=%%p "') do (set "PIDs=%%l")
@@ -275,8 +271,8 @@ call echo %date% %time:~0,8%	Starting %%server%var%%% Server...
 call cd /d %%serverpath%var%%%
 call start /min .\Expansion\beta\arma2oaserver.exe %%serverstart%var%%%
 timeout /t 1 /nobreak > nul
-for /f "skip=3 tokens=2" %%p in ('tasklist /fi "imagename eq %ARMA2%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDSERVER%var% set "PIDSERVER%var%=%%p")
-if defined PIDSERVER%var% call echo %date% %time:~0,8%	%%server%var%%% started with PID: %%PIDSERVER%var%%%
+for /f "skip=3 tokens=2" %%p in ('tasklist /fi "imagename eq %ARMA2%"') do (echo %PIDs% | find "%%p" > nul || if not defined pidserver%var% set "pidserver%var%=%%p")
+if defined pidserver%var% call echo %date% %time:~0,8%	%%server%var%%% started with PID: %%pidserver%var%%%
 timeout /t %timeoutRE% /nobreak > nul
 call echo %date% %time:~0,8%	Starting BEC for %%server%var%%% Server... 
 timeout /t 1 /nobreak > nul
@@ -284,25 +280,17 @@ for /f "delims=" %%l in ('for /f "skip=3 tokens=2" %%p in ^('call tasklist /fi "
 cd /d %becpath%
 call start /min "%becpath%" "%%bec%var%%%" -f %%server%var%beccfg%%
 timeout /t 1 /nobreak > nul
-for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || if not defined PIDBEC%var% set "PIDBEC%var%=%%p")
-if defined PIDBEC%var% call echo %date% %time:~0,8%	BEC started with PID: %%PIDBEC%var%%%
+for /f "skip=3 tokens=2" %%p in ('call tasklist /fi "imagename eq %%bec%var%%%"') do (echo %PIDs% | find "%%p" > nul || if not defined pidbec%var% set "pidbec%var%=%%p")
+if defined pidbec%var% call echo %date% %time:~0,8%	BEC started with PID: %%pidbec%var%%%
 timeout /t 1 /nobreak > nul
 cd /d %becpath%
 echo useless > %checkfile%%var%
-
 ::if defined server%var%started call echo %date% - %time:~0,8% -  %%server%var%%% Server started... %%server%var%started%%
 ::if defined server%var%online call echo %%server%var%online%%
-
-::if '%entry%' == '1' goto random
-::if '%entry%' == 'Y' set "entry="
-::if '%entry%' == 'y' set "entry="
-::if '%entry%' == 'YN' set /a var=%var%+1 > nul
-::if '%entry%' == 'yn' goto startserver
-::if '%entry%' == 'N' set "entry="
-::if '%entry%' == 'n' set "entry="
 echo\
 echo --------------------------------------------------------------------------------
-set COUNT=1
+if '%choice%' EQU '%var%' goto gui
+set BECCOUNT=1
 set var=1
 goto check
 :://////////////////////////////////////////////////////////////////////////////////::
@@ -312,11 +300,11 @@ goto check
 cls
 echo Do you wanna create "config_new.ini"?
 set /P config=(Y)es / (N)o? 
-if '%config%' == 'Y' goto configcreateJ
-if '%config%' == 'y' goto configcreateJ
-if '%config%' == 'N' goto exitbatch
-if '%config%' == 'n' goto exitbatch
-if '%config%' == '' goto exitbatch
+if '%config%' EQU 'Y' goto configcreateJ
+if '%config%' EQU 'y' goto configcreateJ
+if '%config%' EQU 'N' goto exitbatch
+if '%config%' EQU 'n' goto exitbatch
+if '%config%' EQU '' goto exitbatch
 :configcreateJ
 cls
 set /P numm=Enter the number of server: 
@@ -340,7 +328,7 @@ echo server%var%offline=>> config_new.ini
 echo server%var%online=>> config_new.ini
 echo server%var%started=>> config_new.ini
 echo\ >> config_new.ini
-if '%numm%' == '%var%' goto exitbatch
+if '%numm%' EQU '%var%' goto exitbatch
 set /a var=%var%+1 > nul
 goto servervar
 :exitbatch
@@ -352,8 +340,8 @@ exit
 :://///RESTARTING SCRIPT IF AUTO=Y//////////////////////////////////////////////////::
 :://////////////////////////////////////////////////////////////////////////////////::
 :restartarmare
-if '%auto%' == 'N' set timer=1
-if '%auto%' == 'N' goto check
+if '%auto%' EQU 'N' set timer=1
+if '%auto%' EQU 'N' goto check
 cd /d %STARTPATH%
 start /I "" ARMARE.bat
 taskkill /f /pid %PID% > nul
